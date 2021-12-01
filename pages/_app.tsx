9b9fff7fdd481c77674b7584dd33c393f50a5570
lakardion <lakardion@protonmail.com>
2021-11-30 23:10:23 -0300

    intial app commit

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import { AppProps } from "next/app";
+import Layout from "../components/layout/Layout";
+import "../styles/globals.scss";
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />;
+    </Layout>
+  );
+}
+
+export default App;
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+const Home = () => {
+  return (
+    <div>
+      Usá nuestra barra de búsqueda y descubrí con qué sorprenderte en tu puerta
+    </div>
+  );
+};
+export default Home;
